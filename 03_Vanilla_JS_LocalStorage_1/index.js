e90fed250e3d6c8fe3367a3d7efbc8ade39e1cc5
wilderif <woojuro3@gmail.com>
2024-08-07 17:47:24 +0900

    feat(03_Vanilla_JS_LocalStorage_1): Update card rendering logic based on index
    - Updated logic to render cards based on index for improved rendering approach

--- 03_Vanilla_JS_LocalStorage_1/index.js
+++ 03_Vanilla_JS_LocalStorage_1/index.js
@@ -1,3 +1,5 @@
+// localStorage의 data와 cards 변수의 데이터 동기화 시점 전부 다시 확인할 것
+
 const modal = document.querySelector(".modal");
 const modalOpenBtn = document.getElementById("add-card-btn");
 const modalCloseBtn = document.getElementById("modal-close");
@@ -8,7 +10,7 @@ const nextBtn = document.getElementById("next-btn");
 const clearBtn = document.getElementById("clear-btn");
 const cardCount = document.getElementById("card-count");
 
-const cards = JSON.parse(localStorage.getItem("cards")) || [];
+let cards = JSON.parse(localStorage.getItem("cards")) || [];
 
 // 현재 인덱스도 스토리지에 저장해볼 것
 let curCardIdx = 0;
@@ -22,21 +24,24 @@ const updateCardCount = () => {
 };
 
 // 새로운 카드 localStorage에 저장하는 함수
-const addCard = () => {};
+const addCard = (question, answer) => {
+  cards.push({ question, answer });
+  localStorage.setItem("cards", JSON.stringify(cards));
+};
 
 // 카드 div 생성 및 addEventListener
 // index를 parameter로 받아서 활용하도록 수정
-const createCard = () => {
+const createCard = (idx) => {
   const newCard = document.createElement("div");
   newCard.className = "card";
   newCard.innerHTML = `
       <div class="front">
         <p>클릭하면 뒤집혀요</p>
-        <p>${cards[curCardIdx].question}</p>
+        <p>${cards[idx].question}</p>
       </div>
       <div class="back">
         <p>클릭하면 뒤집혀요</p>
-        <p>${cards[curCardIdx].answer}</p>
+        <p>${cards[idx].answer}</p>
       </div>
     `;
   cardContainer.appendChild(newCard);
@@ -49,11 +54,18 @@ const createCard = () => {
 
 // 현재 index -1 ~ +1 까지 화면에 랜더링 하고,
 // 현재 index가 가운데 오도록
-const renderCard = () => {
+const renderCard = (curIdx) => {
   cardContainer.innerHTML = "";
-  if (cards.length >= 3) {
-  } else if (cards.length == 2) {
-  } else if (cards.length == 1) {
+  if (curIdx == -1) return;
+
+  // 카드 개수에 따라 다른 UI 적용해볼 것
+  // if (cards.length >= 3) {
+  // } else if (cards.length == 2) {
+  // } else if (cards.length == 1) {
+  // }
+
+  for (let i = 0; i < 3; i++) {
+    createCard(curIdx + (i % cards.length));
   }
 };
 
@@ -70,16 +82,20 @@ const move = (flag) => {
     cardContainer.style.transform = "translateX(calc(100% / -3))";
 
     // 조건문 내부에 Index 업데이트 추가할 것
-    // element 붙이는 방식 아닌 제거하고 다음 Index 추가하는 방식으로
     // Index만 변경하고 renderCard 함수 사용하는 방법으로 수정
     if (flag) {
-      const firstCard = cardContainer.firstElementChild;
-      slideContainer.appendChild(firstCard);
+      curCardIdx = (curCardIdx + 1) % cards.length;
+      renderCard(curCardIdx);
     } else {
-      const lastCard = cardContainer.lastElementChild;
-      slideContainer.prepend(lastCard);
+      curCardIdx = (curCardIdx - 1 + cards.length) % cards.length;
+      renderCard(curCardIdx);
     }
   }, 600);
+};
+
+const handleCloseModal = () => {
+  addCardForm.reset();
+  modal.style.display = "none";
 };
 
 modalOpenBtn.addEventListener("click", () => {
@@ -89,21 +105,19 @@ modalOpenBtn.addEventListener("click", () => {
 // modal content 바깥 부분 눌렀을 때도 같은 동작 하도록 추가해볼 것
 // 모달 닫는 동작 함수로 따로 분리
 modalCloseBtn.addEventListener("click", () => {
-  addCardForm.reset();
-  modal.style.display = "none";
+  handleCloseModal();
 });
 
 addCardForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const question = document.getElementById("question").value;
   const answer = document.getElementById("answer").value;
-  if (question && answer) {
-    cards.push({ question, answer });
-    localStorage.setItem("cards", JSON.stringify(cards));
-    createCard();
-    modal.style.display = "none";
-    addCardForm.reset();
-  }
+  if (!question || !answer) return;
+
+  addCard(question, answer);
+  curCardIdx++;
+  renderCard(curCardIdx);
+  handleCloseModal();
 });
 
 prevBtn.addEventListener("click", () => {
@@ -117,7 +131,7 @@ nextBtn.addEventListener("click", () => {
 clearBtn.addEventListener("click", () => {
   localStorage.removeItem("cards");
   cards = [];
-  renderCard();
+  renderCard(-1);
   curCardIdx = 0;
   updateCardCount();
 });
